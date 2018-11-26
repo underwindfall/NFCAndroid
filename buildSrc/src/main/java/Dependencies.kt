6220981e478829a2ac2cfec2afbc5c1c001c16d6
Qifan Yang <yangqifan02@hotmail.com>
2018-11-26 14:23:56 +0100

    refactor Dependencies structure

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,18 @@
+object Versions {
+    const val support_lib = "28.0.0"
+    const val constraint_lib = "1.1.3"
+
+    //test unit version
+    const val junit_lib = "4.12"
+    const val testRunner = "1.0.2"
+    const val testEspresso = "3.0.2"
+}
+
+object Libs {
+    val support_appcompat_v7 = "com.android.support:appcompat-v7:${Versions.support_lib}"
+    val support_constraint_layout = "com.android.support.constraint:constraint-layout:${Versions.constraint_lib}"
+    //Test unit
+    val junit = "junit:junit:${Versions.junit_lib}"
+    val test_runner = "com.android.support.test:runner:${Versions.testRunner}"
+    val espresso_core = "com.android.support.test.espresso:espresso-core:${Versions.testEspresso}"
+}
